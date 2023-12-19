922d259c951d366e6b81d34962c0d3f86911ebdc
Iulian-Constantin Marcu <iulian-constantin.marcu@popsa.com>
2023-12-19 16:41:01 +0200

    Solutions for Day 3

---
+++ day_03/part_1.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	inputFile = "day_03/input_1.txt"
+)
+
+type PossibleNumber struct {
+	Value int
+	Row   int
+	Start int
+	End   int
+}
+
+type Symbol struct {
+	Value string
+	Row   int
+	Col   int
+}
+
+func main() {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading file \"%v\"\n", inputFile)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	total := 0
+
+	possibleNumbers := make([]PossibleNumber, 0)
+	symbols := make([]Symbol, 0)
+
+	l := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		for i := 0; i < len(line); i++ {
+			char := line[i]
+
+			if char != '.' {
+				// can be a symbol or a number
+				if char >= '0' && char <= '9' {
+					// scan until another '.' is found or the end of the line
+
+					j := i
+					for j < len(line) && line[j] >= '0' && line[j] <= '9' {
+						j++
+					}
+
+					start := i
+					end := j - 1
+					i = end
+
+					// get actual value
+					value := 0
+					fmt.Sscanf(line[start:end+1], "%d", &value)
+
+					possibleNumbers = append(possibleNumbers, PossibleNumber{
+						Value: value,
+						Row:   l,
+						Start: start,
+						End:   end,
+					})
+				} else {
+					// this is a symbol
+					symbols = append(symbols, Symbol{
+						Value: string(char),
+						Row:   l,
+						Col:   i,
+					})
+
+					fmt.Printf("Symbol: %v %v %v\n", string(char), l, i)
+				}
+			}
+		}
+
+		l++
+	}
+
+	// now we have all the possible numbers and symbols
+	// we need to figure out which numbers are valid (adjacent to symbols)
+	// and which are not
+	for _, number := range possibleNumbers {
+		valid := false
+
+		for _, symbol := range symbols {
+			if symbol.Row == number.Row {
+				// same row
+				if symbol.Col == number.Start-1 || symbol.Col == number.End+1 {
+					valid = true
+					break
+				}
+			} else if symbol.Row == number.Row-1 || symbol.Row == number.Row+1 {
+				// adjacent row
+				if symbol.Col >= number.Start-1 && symbol.Col <= number.End+1 {
+					valid = true
+					break
+				}
+			}
+		}
+
+		if valid {
+			fmt.Printf("Valid number: %v %v %v\n", number.Value, number.Start, number.End)
+			total += number.Value
+		}
+	}
+
+	fmt.Printf("Total is: %v\n", total)
+}
---
+++ day_03/part_2.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	inputFile = "day_03/input_2.txt"
+)
+
+type PossibleNumber struct {
+	Value int
+	Row   int
+	Start int
+	End   int
+}
+
+type Symbol struct {
+	Value string
+	Row   int
+	Col   int
+}
+
+func main() {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading file \"%v\"\n", inputFile)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	total := 0
+
+	possibleNumbers := make([]PossibleNumber, 0)
+	possibleGears := make([]Symbol, 0)
+
+	l := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		for i := 0; i < len(line); i++ {
+			char := line[i]
+
+			if char != '.' {
+				// can be a symbol or a number
+				if char >= '0' && char <= '9' {
+					// scan until another '.' is found or the end of the line
+
+					j := i
+					for j < len(line) && line[j] >= '0' && line[j] <= '9' {
+						j++
+					}
+
+					start := i
+					end := j - 1
+					i = end
+
+					// get actual value
+					value := 0
+					fmt.Sscanf(line[start:end+1], "%d", &value)
+
+					possibleNumbers = append(possibleNumbers, PossibleNumber{
+						Value: value,
+						Row:   l,
+						Start: start,
+						End:   end,
+					})
+				} else {
+					if char != '*' {
+						continue
+					}
+
+					// this is a possible gear
+					possibleGears = append(possibleGears, Symbol{
+						Value: string(char),
+						Row:   l,
+						Col:   i,
+					})
+
+					fmt.Printf("Possible Gear: %v %v %v\n", string(char), l, i)
+				}
+			}
+		}
+
+		l++
+	}
+
+	// now we have all the possible numbers and possibleGears
+	// we need to figure out which ones are the gears and which numbers are adjacent to them
+
+	for _, symbol := range possibleGears {
+		adjacentNumbers := []PossibleNumber{}
+
+		for _, number := range possibleNumbers {
+			if symbol.Row == number.Row {
+				// same row
+				if symbol.Col == number.Start-1 || symbol.Col == number.End+1 {
+					adjacentNumbers = append(adjacentNumbers, number)
+				}
+			} else if symbol.Row == number.Row-1 || symbol.Row == number.Row+1 {
+				// adjacent row
+				if symbol.Col >= number.Start-1 && symbol.Col <= number.End+1 {
+					adjacentNumbers = append(adjacentNumbers, number)
+				}
+			}
+		}
+
+		valid := len(adjacentNumbers) == 2
+
+		if valid {
+			total += adjacentNumbers[0].Value * adjacentNumbers[1].Value
+		}
+	}
+
+	fmt.Printf("Total is: %v\n", total)
+}
